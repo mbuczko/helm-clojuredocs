bd3ae2f578be23457ded103b485a41fa0ae272a7
Michal Buczko <michal.buczko@gmail.com>
2016-02-21 00:50:47 +0100

    caching results within single helm session
    helm-clojuredocs-at-point added

--- helm-clojuredocs.el
+++ helm-clojuredocs.el
@@ -3,7 +3,7 @@
 ;; Author: Michal Buczko <michal.buczko@gmail.com>
 ;; URL: https://github.com/mbuczko/helm-clojuredocs
 ;; Package-Requires: ((edn "1.1.2") (helm "1.5.7"))
-;; Version: 0.1
+;; Version: 0.2
 ;; Keywords: helm, clojure
 
 ;; Copyright (C) 2016 Michal Buczko <michal.buczko@gmail.com>
@@ -64,20 +64,20 @@
              collect (helm-clojuredocs--parse-suggestion i) into result
              finally return result)))
 
-(defun helm-clojuredocs-fetch (input)
+(defun helm-clojuredocs-fetch (pattern request-prefix)
   "Fetch Clojuredocs suggestions and return them as a list."
   (let ((request (concat helm-clojuredocs-suggest-url
-                         (url-hexify-string input))))
-    (helm-net--url-retrieve-sync
-     request #'helm-clojuredocs--parse-buffer)))
+                         (url-hexify-string (or (and request-prefix
+                                                     (concat request-prefix " " pattern))
+                                                pattern)))))
+    (puthash pattern (helm-net--url-retrieve-sync
+                      request #'helm-clojuredocs--parse-buffer) helm-clojuredocs-cache)))
 
 (defun helm-clojuredocs-set-candidates (&optional request-prefix)
   "Set candidates with result and number of clojuredocs results found."
-  (let ((suggestions (helm-clojuredocs-fetch
-                      (or (and request-prefix
-                               (concat request-prefix
-                                       " " helm-pattern))
-                          helm-pattern))))
+  (let ((suggestions (or (gethash helm-pattern helm-clojuredocs-cache)
+                         (or (sleep-for 0.1)
+                             (helm-clojuredocs-fetch helm-pattern request-prefix)))))
     (if (member helm-pattern suggestions)
         suggestions
       (append
@@ -85,20 +85,36 @@
        (list (cons (format "Search for '%s' on clojuredocs.org" helm-input)
                    (concat "/search?q=" helm-input)))))))
 
+(defvar helm-clojuredocs-cache (make-hash-table :test 'equal))
 (defvar helm-source-clojuredocs
   (helm-build-sync-source "clojuredocs.org suggest"
     :candidates #'helm-clojuredocs-set-candidates
+    :init (lambda () (clrhash helm-clojuredocs-cache))
     :action '(("Go to clojuredocs.org" . (lambda (candidate)
                                            (browse-url (concat "http://clojuredocs.org" candidate)))))
     :volatile t
     :requires-pattern 3))
 
+
+(defun helm-clojuredocs-invoke (&optional init-value)
+  (setq debug-on-error t)
+  (helm :sources 'helm-source-clojuredocs
+        :buffer "*helm-clojuredocs*"
+        :prompt "Doc for: "
+        :input init-value)
+  (setq debug-on-error nil))
+
 ;;;###autoload
 (defun helm-clojuredocs ()
   "Preconfigured `helm' for searching in clojuredocs.org"
   (interactive)
-  (setq debug-on-error t)
-  (helm-other-buffer 'helm-source-clojuredocs "*helm clojuredocs*"))
+  (helm-clojuredocs-invoke))
+
+;;;###autoload
+(defun helm-clojuredocs-at-point ()
+  "Preconfigured `helm' for searching in clojuredocs.org with symbol at point"
+  (interactive)
+  (helm-clojuredocs-invoke (thing-at-point 'symbol)))
 
 (provide 'helm-clojuredocs)
 
